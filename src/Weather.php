93908b845e75ea3b467c84c3c09ee4bc070a8085
vagrant <vagrant@homestead>
2018-10-26 05:28:30 +0000

    Add travis ci config file.

--- src/Weather.php
+++ src/Weather.php
@@ -10,7 +10,7 @@ class Weather
     protected $key;
     protected $guzzleOptions = [];
 
-    public function __construct(string $key)
+    public function __construct($key)
     {
         $this->key = $key;
     }
@@ -35,7 +35,7 @@ class Weather
         return $this->getWeather($city, 'all', $format);
     }
 
-    public function getWeather($city, string $type = 'base', string $format = 'json')
+    public function getWeather($city, $type = 'base', $format = 'json')
     {
 
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
