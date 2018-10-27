53792c3ed25b5509094e6f053b28b60bf94e9df1
yueruitao <1316751406@qq.com>
2018-10-27 01:50:11 +0000

    Apply fixes from StyleCI

--- tests/WeatherTest.php
+++ tests/WeatherTest.php
@@ -2,6 +2,11 @@
 
 /*
  * This file is part of the yueruitao/weather.
+ *
+ * (c) yueruitao <i@yueruitao.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Yueruitao\Weather\Tests;
